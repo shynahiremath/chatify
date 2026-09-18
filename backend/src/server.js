2020d431f81f724358813d8b7b02cfefca90e817
shnynahiremath <shynahiremath18@gmail.com>
2026-09-18 09:56:27 +0530

    updatedprofile implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
@@ -13,6 +13,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
