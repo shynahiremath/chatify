1db10ebd2ed4553c58d1d5be78e7cceec23ff7a4
shnynahiremath <shynahiremath18@gmail.com>
2026-09-19 12:30:27 +0530

    sidebar completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,8 +12,8 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = create((set) => ({
   isCheckingAuth: true,
   isSigningUp: false,
   isLoggingIn: false,
+  isUpdatingProfile: false,
 
   checkAuth: async () => {
   try {
@@ -57,4 +58,19 @@ logout: async () => {
     console.log("Logout error:", error);
   }
 },
+
+updateProfile: async (data) => {
+  set({ isUpdatingProfile: true });
+  try {
+    const res = await axiosInstance.put("/auth/update-profile", data);
+    set({ authUser: res.data });
+    toast.success("Profile updated successfully");
+  } catch (error) {
+    console.log("error in update profile:", error);
+    toast.error(error.response?.data?.message || "Something went wrong");
+  } finally {
+    set({ isUpdatingProfile: false });
+  }
+},
+
 }));
